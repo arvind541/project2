461b0f5a68335f252ba10a7dae27a3d730748f3d
arvind541 <62004094+arvind541@users.noreply.github.com>
2020-08-29 16:38:19 +0530

    Add files via upload

---
+++ issue-module.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+/** @title Drawer with explicit backdrop setting */
+@Component({
+  selector: 'sidenav-backdrop-example',
+  templateUrl: 'sidenav-backdrop-example.html',
+  styleUrls: ['sidenav-backdrop-example.css'],
+})
+export class SidenavBackdropExample {}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
